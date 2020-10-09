ccbaa716472e6237743864b4f7added6a4bdc93f
HappyFireDevs <67799431+HappyFireDevs@users.noreply.github.com>
2020-10-09 17:47:23 -0600

    uploaded from computer

--- 2020_election_results_map.js
+++ 2020_election_results_map.js
@@ -1,9 +1,9 @@
 (function () {
+    // Create the connector object
     var myConnector = tableau.makeConnector();
 
+    // Define the schema
     myConnector.getSchema = function (schemaCallback) {
-
-        tableau.log("Hello Alexander from WDC!");
         var cols = [{
             id: "statePostal",
             alias: "State Postal",
@@ -17,9 +17,9 @@
         };
 
         schemaCallback([tableSchema]);
-
     };
 
+    // Download the data
     myConnector.getData = function (table, doneCallback) {
         $.getJSON("https://api.ap.org/v2/elections/2020-11-03?statepostal=WI&resultstype=t&level=ru&format=json&apikey=bAcPmpDWG3q7sxzFnD5QB6EvIpwIfmeT&officeID=P&raceTypeId=G", function (resp) {
             var feat = resp.features,
@@ -28,7 +28,7 @@
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "statePostal": feat[i].statePostal,
+                    "statePostal": feat[i].statePostal
                 });
             }
 
@@ -38,11 +38,12 @@
     };
 
     tableau.registerConnector(myConnector);
-})();
 
-$(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "2020 Election Test Data!";
-        tableau.submit();
+    // Create event listeners for when the user submits the form
+    $(document).ready(function () {
+        $("#submitButton").click(function () {
+            tableau.connectionName = "2020 Election Test Data!"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
     });
-});
+})();
